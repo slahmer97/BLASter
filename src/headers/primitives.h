dad6f36fce6c1db0357ee5e68af363a763eb724b
Lahmer Seyyid-Ahmed <seyyidahmed.lahmer@etu.unistra.fr>
2020-01-12 13:06:08 +0100

    fixed.......

---
+++ src/headers/primitives.h
@@ -0,0 +1,15 @@
+//
+// Created by slahmer on 1/5/20.
+//
+
+#ifndef BLASTER_PRIMITIVES_H
+#define BLASTER_PRIMITIVES_H
+
+
+void init_fvec(const int N,float *X,float val);
+
+
+
+
+
+#endif //BLASTER_PRIMITIVES_H
---
+++ src/primitives.c
@@ -0,0 +1,9 @@
+//
+// Created by slahmer on 1/5/20.
+//
+
+#include "headers/primitives.h"
+void init_fvec(const int N,float *X,float val){
+    for (int i = 0; i < N ; ++i)
+        X[i] = val;
+}
